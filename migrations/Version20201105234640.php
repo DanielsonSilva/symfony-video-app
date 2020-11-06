bb794ae2e9a2db2e7fbfa635cb2f327aa8feb0bf
Danielson Flávio Xavier da Silva <danielson_fx@yahoo.com.br>
2020-11-05 21:24:43 -0300

    Add customized nav bar with categories from fixtures inside database and created a twig filter to adjust the url from categories

--- migrations/Version20201105234640.php
+++ migrations/Version20201105234640.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201104000809 extends AbstractMigration
+final class Version20201105234640 extends AbstractMigration
 {
     public function getDescription() : string
     {
--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -11,13 +11,34 @@ class CategoryFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $this->loadMainCategories($manager);
+        $this->loadSubCategories($manager, 'Eletronics', 1);
+        $this->loadSubCategories($manager, 'Computers', 6);
+        $this->loadSubCategories($manager, 'Laptops', 9);
+        $this->loadSubCategories($manager, 'Books', 2);
+        $this->loadSubCategories($manager, 'Movies', 4);
+        $this->loadSubCategories($manager, 'Romance', 19);
     }
     
     private function loadMainCategories($manager)
     {
-        foreach($this->getCategoriesData() as $name) {
+        foreach($this->getCategoriesData() as [$name]) {
             $category = new Category();
             $category->setName($name);
+            $manager->persist($category);
+            
+            $manager->flush();
+        }
+    }
+    
+    private function loadSubCategories($manager, $category_name, $parent_id)
+    {
+        $parent = $manager->getRepository(Category::class)->find($parent_id);
+        $method = "get{$category_name}Data";
+        
+        foreach($this->$method() as [$name]) {
+            $category = new Category();
+            $category->setName($name);
+            $category->setParent($parent);
             $manager->persist($category);
             
             $manager->flush();
@@ -27,20 +48,63 @@ class CategoryFixtures extends Fixture
     private function getCategoriesData(): array
     {
         return [
-            'Eletronics',
-            'Books',
-            'Toys',
-            'Movies'
-        ];
-    }
-    
-    private function getCategoriesIds(): array
-    {
-        return [
             ['Eletronics', 1],
             ['Books', 2],
             ['Toys', 3],
             ['Movies', 4]
+        ];
+    }
+    
+    private function getEletronicsData(): array
+    {
+        return [
+            ['Cameras', 5],
+            ['Computers', 6],
+            ['Cellphones', 7],
+            ['Videogames', 8]
+        ];
+    }
+    
+    private function getComputersData(): array
+    {
+        return [
+            ['Laptops', 9],
+            ['Desktops', 10]
+        ];
+    }
+    
+    private function getLaptopsData(): array
+    {
+        return [
+            ['Apple', 11],
+            ['Asus', 12],
+            ['Dell', 13],
+            ['Lenovo', 14],
+            ['HP', 15]
+        ];
+    }
+    
+    private function getBooksData(): array
+    {
+        return [
+            ['Children', 16],
+            ['Kindle', 17]
+        ];
+    }
+    
+    private function getMoviesData(): array
+    {
+        return [
+            ['Family', 18],
+            ['Romance', 19]
+        ];
+    }
+    
+    private function getRomanceData(): array
+    {
+        return [
+            ['Romantic Comedy', 20],
+            ['Romantic Drama', 21]
         ];
     }
 }
